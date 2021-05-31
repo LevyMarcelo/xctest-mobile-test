262ebee462c242ea9313ebd0b55b5c8812a6fa65
Levy Cunha <levy.cunha@e-unicred.com.br>
2021-05-31 02:42:51 -0300

    Update PersonalDataScreen

--- OnboardingUITests/Screens/PersonalDataScreen.swift
+++ OnboardingUITests/Screens/PersonalDataScreen.swift
@@ -5,6 +5,8 @@ class PersonalDataScreen: Utils {
     
     let cpfField = "Número do CPF"
     let fullNameField = "Nome completo"
+    let whatIsSocialNameButton = "?"
+    let okUnderstoodButton = "Certo, entendi!"
     
     func fillCpfField() {
         write(element: cpfField, value: "443.388.000-03")
@@ -15,9 +17,9 @@ class PersonalDataScreen: Utils {
     }
     
     func whatIsSocialName() {
-        click(element: "?")
+        click(element: whatIsSocialNameButton)
         textValidation(text: "O que é um nome social?")
-        click(element: "Certo, entendi!")
+        click(element: okUnderstoodButton)
     }
     
     func buttonValidationContinue() {
